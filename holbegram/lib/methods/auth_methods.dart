fc3e1d07a050474413c4a845babda557c4eebc4b
JustGodWork <discord1elie@gmail.com>
2025-12-18 14:43:06 +0100

    feat(auth): add getUserDetails method to retrieve current user information

--- holbegram/lib/methods/auth_methods.dart
+++ holbegram/lib/methods/auth_methods.dart
@@ -61,4 +61,11 @@ class AuthMethode {
       return e.toString();
     }
   }
+
+  Future<Users> getUserDetails() async {
+    User currentUser = _auth.currentUser!;
+    DocumentSnapshot snap =
+        await _firestore.collection('users').doc(currentUser.uid).get();
+    return Users.fromSnap(snap);
+  }
 }
