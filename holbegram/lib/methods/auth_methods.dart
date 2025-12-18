7f8a2d7c454307fa767d45dba28b89603c5e2939
JustGodWork <discord1elie@gmail.com>
2025-12-18 13:44:54 +0100

    feat(auth): implement authentication methods for login and sign up

---
+++ holbegram/lib/methods/auth_methods.dart
@@ -0,0 +1,64 @@
+import 'dart:typed_data';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:holbegram/models/user.dart';
+
+class AuthMethode {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Future<String> login({
+    required String email,
+    required String password,
+  }) async {
+    if (email.isEmpty || password.isEmpty) {
+      return 'Please fill all the fields';
+    }
+    try {
+      await _auth.signInWithEmailAndPassword(email: email, password: password);
+      return 'success';
+    } catch (e) {
+      return e.toString();
+    }
+  }
+
+  Future<String> signUpUser({
+    required String email,
+    required String password,
+    required String username,
+    Uint8List? file,
+  }) async {
+    if (email.isEmpty || password.isEmpty || username.isEmpty) {
+      return 'Please fill all the fields';
+    }
+    try {
+      UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      User? user = userCredential.user;
+
+      // For now, we'll use a placeholder for the photoUrl
+      String photoUrl = '';
+
+      Users users = Users(
+        uid: user!.uid,
+        email: email,
+        username: username,
+        bio: '',
+        photoUrl: photoUrl,
+        followers: [],
+        following: [],
+        posts: [],
+        saved: [],
+        searchKey: username.substring(0, 1).toUpperCase(),
+      );
+
+      await _firestore.collection("users").doc(user.uid).set(users.toJson());
+      return 'success';
+    } catch (e) {
+      return e.toString();
+    }
+  }
+}
