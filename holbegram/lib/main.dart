a4ce856ca5ee36ec4b085774a5b8efc616f439e2
JustGodWork <discord1elie@gmail.com>
2025-12-18 14:41:07 +0100

    feat(main): update home screen to use LoginScreen instead of placeholder text

--- holbegram/lib/main.dart
+++ holbegram/lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
+import 'package:holbegram/screens/login_screen.dart';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
@@ -16,10 +18,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      title: 'Holbegram',
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(child: Text('Holbegram ready')),
-      ),
+      home: LoginScreen()
     );
   }
 }
